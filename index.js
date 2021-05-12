fde09ca155aa515c98debd725363f8b662e76b61
UglyRatCode <78694553+UglyRatCode@users.noreply.github.com>
2021-05-12 22:40:41 +0100

    some session logic added,, not fully working

--- index.js
+++ index.js
@@ -13,24 +13,85 @@ var server = app.listen(glblPort, () => { console.log('the server is now running
 
 var io = require('socket.io')(server);
 var activesessions = [];
+var activegames = [];
 
+//session middleware
+io.use((socket,next)=>
+    {
+        //if none, give it a session ID
+        if (!socket.handshake.auth.seshID){
+            socket.seshID = newSessionID();
+            activegames.push(socket.seshID);
+        } else socket.seshID = socket.handshake.auth.seshID;
+        next();
+    }
+);
+
+//socket handler
 io.on('connection', (socket)=>{
-    socket.on('join',(room)=>{console.log(room)});
+    socket.emit('session',socket.seshID);
+    socket.on('join',(room,response)=>{
+        if(roomExists(room)){
+            socket.join(room);
+            io.to(room).emit('new connection');
+            response({status: 200});
+        } else response({status: 404});
+    });
     socket.on('new', (response) => {
-        let id = newID();
+        let id = newRoomID();
         if (id == "err") response({status: 404});
         else{
-            activesessions.push(id);
-            socket.join(id);
-            response({status:200, code:id});
+            let newgame = new game(id,socket,socket,0);
+            activegames.push(newgame);
+            socket.join(newgame.roomID);
+            response({status:200, code:newgame.roomID});
     }});
 });
 
-function newID(){
+function newRoomID(){
     let randID = Math.random().toString(36).slice(2,8);
+    if(activegames.indexOf(randID) > -1)
+        newID();
+    else if (activegames.length > 10000)
+        return "err";
+    else return randID;
+};
+
+function newSessionID(){
+    let randID = Math.random().toString(36).slice(2,12);
     if(activesessions.indexOf(randID) > -1)
         newID();
     else if (activesessions.length > 10000)
         return "err";
     else return randID;
+};
+
+function roomExists(id){
+    for (let i=0;i<activegames.length;i++){
+        if (activegames[i].roomID == id)
+            return true;
+    }
+    return false;
+}
+class game {
+    constructor(roomID,players,host,gameState){
+        this.roomID = roomID;
+        this.players = players;
+        this.host = host;
+        this.gameState = gameState;
+    }
+    
+    addPlayer(player){
+        this.players.push(player);
+    }
+    removePlayer(player){
+
+    }
+};
+
+class player {
+    constructor(sessionID, username){
+        this.sessionID = sessionID;
+        this.username = username;
+    }
 };
--- public/client.js
+++ public/client.js
@@ -1,4 +1,9 @@
-var socket = io();
+var socket = io({autoConnect: false});
+
+//get cookie mmm
+var sessionID = localStorage.getItem('sessionID');
+if(sessionID) socket.auth = {seshID: sessionID};
+socket.connect();
 
 //set up elements
 
@@ -10,21 +15,35 @@ var testDiv = document.getElementById('teststuff');
 //set up other variables
 
 var myRoom;
+console.log(JSON.stringify(socket.auth.seshID));
 
 //set up DOM bits
 var roomDisplay = document.createElement('p');
 roomDisplay.innerHTML = 'Room Code: ';
 testDiv.appendChild(roomDisplay);
 
+//event listeners
 join.addEventListener('click', () => {socket.emit('join',roomInput.value,(response) => {
     if(response.status == 404) alert('No room found');
+    else setRoom(response.code);
 });
 });
+
 news.addEventListener('click',() => {socket.emit('new',(response) => {
     if (response.status == 404) alert ('Room could not be created');
-    else{ 
-        myRoom = response.code;
-        roomDisplay.innerHTML = 'Room Code: ' + myRoom;
-    }
+    else setRoom(response.code);
 });
 });
+
+//socket listeners
+socket.on('session',(sessionID) => {
+    socket.auth = {seshID: sessionID};
+    localStorage.setItem('sessionID',sessionID);
+});
+
+//functions
+
+function setRoom(roomCode){
+    myRoom = roomCode;
+    roomDisplay.innerHTML = 'Room Code: ' + myRoom;
+}
