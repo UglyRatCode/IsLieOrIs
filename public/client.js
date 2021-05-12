e47784af28159ae404fb094947a32defb701ec20
UglyRatCode <78694553+UglyRatCode@users.noreply.github.com>
2021-05-12 20:55:34 +0100

    moved where testdiv went oops

--- public/client.js
+++ public/client.js
@@ -5,11 +5,16 @@ var socket = io();
 var join  = document.getElementById('joinSesh');
 var news  = document.getElementById('newSesh');
 var roomInput = document.getElementById('roomcode');
+var testDiv = document.getElementById('teststuff');
 
 //set up other variables
 
 var myRoom;
 
+//set up DOM bits
+var roomDisplay = document.createElement('p');
+roomDisplay.innerHTML = 'Room Code: ';
+testDiv.appendChild(roomDisplay);
 
 join.addEventListener('click', () => {socket.emit('join',roomInput.value,(response) => {
     if(response.status == 404) alert('No room found');
