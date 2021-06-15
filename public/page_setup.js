8eed5020dd0b010677801288a8323b349d8efd1e
Lithren <niallj1996@gmail.com>
2021-06-15 21:25:18 +0100

    Bettered

--- public/page_setup.js
+++ public/page_setup.js
@@ -12,21 +12,21 @@ function main(){
 
 function colourButtonSetup(){
     for (const[key, value] of Object.entries(colours)) {
-        let cB = document.createElement("BUTTON");
-        cB.setAttribute("id", `clr-${key}`);
-        cB.addEventListener("click", ()=> changeStrokeColour(key));
-        cB.style.background = value;
-        document.getElementById("colour-buttons").appendChild(cB);
+        let colourButton = document.createElement("BUTTON");
+        colourButton.setAttribute("id", `clr-${key}`);
+        colourButton.addEventListener("click", ()=> changeStrokeColour(key));
+        colourButton.style.background = value;
+        document.getElementById("colour-buttons").appendChild(colourButton);
     }
 }
 
 function strokeWidthButtonSetup(){
-    for (const[key, value] of Object.entries(thicnesses)) {
-        let cB = document.createElement("BUTTON");
-        cB.setAttribute("id", `thc-${key}`);
-        cB.addEventListener("click", ()=> changeStrokeThiccness(key));
-        cB.textContent = key;
-        document.getElementById("thc-buttons").appendChild(cB);
+    for (const[key] of Object.entries(thicnesses)) {
+        let widthButton = document.createElement("BUTTON");
+        widthButton.setAttribute("id", `thc-${key}`);
+        widthButton.addEventListener("click", ()=> changeStrokeThiccness(key));
+        widthButton.textContent = key;
+        document.getElementById("thc-buttons").appendChild(widthButton);
     }
 }
 
@@ -40,5 +40,15 @@ function setupGeneralListeners(){
     canvas.addEventListener('touchstart', stopDraw, {passive: true});
 }
 
-main();
+//General function for quickly creating multiple elements from array.
+function generateElementsFromArray(inputArray, elementToCreate, createdIDName, parentElementID){
+    if(inputArray == null || elementToCreate == null || createdIDName == null || parentElementID == null){return;};
 
+    for (let i = 0; i < inputArray.length; i++){
+        let newElement = document.createElement(elementToCreate);
+        newElement.setAttribute("id", `${createdIDName}-${i + 1}`);
+        document.getElementById(parentElementID).appendChild(newElement);
+    }
+}
+
+main();
